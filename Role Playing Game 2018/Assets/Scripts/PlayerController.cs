356b958f11005bc2ada875ef0e2055299fdd7c7e
Muhammad Murtaza <mmz2412150@gmail.com>
2023-07-03 18:47:44 +0500

    Imported Assets and Created the PlayerController  Script

---
+++ Role Playing Game 2018/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+[RequireComponent(typeof(Animator))]
+public class PlayerController : MonoBehaviour
+{
+    [Header("References")]
+    public Animator animator;
+    public CharacterController controller;
+
+    [Header("Movement")]
+    public float speed = 5f;
+    public float jumpHeight = 3f;
+    public float gravity = -9.81f;
+
+    [Header("Ground Check")]
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    Vector3 velocity;
+    bool isGrounded;
+
+    public const string animatorWalkParameter = "Speed";
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        HandleMovement();
+    }
+
+    void HandleMovement() 
+    {
+        // Check if the player is on the ground
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        // Reset the velocity if the player is on the ground
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        // Get the horizontal and vertical inputs
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        // Move the player
+        Vector3 move = transform.right * horizontal + transform.forward * vertical;
+        controller.Move(move * speed * Time.deltaTime);
+
+        // Set the speed parameter in the animator
+        animator.SetFloat(animatorWalkParameter, move.magnitude);
+
+        // Jump
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            Debug.Log("Player Jumped!");
+        }
+
+        // Apply gravity
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
